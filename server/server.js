d52c1b1c90a9b8202fce53fe06c58b43c2109ae9
zeromilerd <59603799+zeromilerd@users.noreply.github.com>
2023-02-19 22:34:03 +0530

    first commit

--- server/server.js
+++ server/server.js
@@ -5,13 +5,13 @@ import { Configuration, OpenAIApi } from 'openai';
 
 dotenv.config();
 
-console.log(process.env.OPENAI_API_KEY)
+//console.log(process.env.OPENAI_API_KEY)
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
-//const openai = new OpenAIApi(configuration);
+const openai = new OpenAIApi(configuration);
 
 const app = express();
 app.use(cors());
