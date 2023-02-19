e35a6c7eeed04b851b731d9b0fe5134becfee5f3
zeromilerd <59603799+zeromilerd@users.noreply.github.com>
2023-02-19 22:04:22 +0530

    first commit

--- client/script.js
+++ client/script.js
@@ -442,7 +442,7 @@ let loadInterval;
   
     // fetch data from server -> bot's response
     try {
-      const response = await fetch('http://localhost:5000', {
+      const response = await fetch('https://casi-live-lpx3.onrender.com', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
